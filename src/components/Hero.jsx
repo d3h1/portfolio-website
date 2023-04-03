2ea868a313510d91eb6e1fa0ef003e6c5700a8d0
d3h1 <denicabaravdic@gmail.com>
2023-04-02 20:27:53 -0700

    Adding new 3D models

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+
+// Our style properties will be from Tailwind CSS - everything can be found at https://tailwindcss.com/docs/
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      {/* Background Image plus our flex box and regular styling */}
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eef]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          {/* Our text for main page */}
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className='text-[#915eff]'>Deni</span></h1>
+          <p className={`${styles.heroSubText} text-white-100`}> I build fullstack applications, machine <br/>learning models, and video games </p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Suspense, suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
+// This will use three js
 const Computers = () => {
+  const computer = useGLTF('./space/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black"/>
+      <pointLight intensity={1} />
+      <primitive object={computer.scene}
+      scale={4}
+      position={[-8, -7.25, -1.5]}
+      rotation={[1, -0.2, -0.1]} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    // Canvas will allow us to draw our 3-D model --- each one of these is needed to display the model you wanna see
+    <Canvas 
+    frameloop='demand' 
+    shadows 
+    camera={{position: [17, 10, 5], fov: 40}}
+    gl={{ preserveDrawingBuffer: true }}>
+      {/* Suspense will load the canvas with the saved properties */}
+      <Suspense>
+        <OrbitControls 
+        // Does not allow zoom, and allows rotating at a certain point
+        enableZoom={false} 
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
