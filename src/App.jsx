b5809240ac0c00a44f90ae48dacb1e0e09199dfd
denicabaravdic <deni@phntmlabs.com>
2023-04-03 20:20:12 -0700

    cleaning up the portfolio formatting, starting contact page

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
           <Experience />
           <Tech />
           <Works />
-          <Feedbacks />
           <div className='relative z=0'>
             <Contact />
             <StarsCanvas />
